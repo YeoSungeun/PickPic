7d5b63024b59a48b79e5f0fa89af4f788ba64cfe
YeoSungeun <dutjddms@naver.com>
2024-07-24 08:04:27 +0900

    [Setting] Color Extension, Enum 생성

---
+++ PickPic/Global/Constant/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  PickPic
+//
+//  Created by 여성은 on 7/24/24.
+//
+
+import UIKit
+
+enum Constant {
+    enum Color {
+        static let blue = UIColor(rgb: 0x186FF2)
+        static let warning = UIColor(rgb: 0xF04452)
+        static let black = UIColor(rgb: 0x000000)
+        static let darkGray = UIColor(rgb: 0x4D5652)
+        static let gray = UIColor(rgb: 0x8C8C8C)
+        static let lightGray = UIColor(rgb: 0xF2F2F2)
+        static let white = UIColor(rgb: 0xFFFFFF)
+    }
+}
